732a5b88869a646ff84dd7820dbb04f3af7dc095
sirlondon <36821072+sirlondon@users.noreply.github.com>
2025-06-20 05:16:18 +0200

    Add files via upload

--- Magic_Card_Finder.py
+++ Magic_Card_Finder.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import pandas as pd
 from enum import Enum
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+from reportlab.pdfgen import canvas # type: ignore
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle # type: ignore
+from reportlab.lib import colors # type: ignore # type: ignore
+from PyPDF2 import PdfMerger # type: ignore
 
 st.set_page_config(page_title="Magic Karten Finder", layout="wide")
 
 st.title("Magic Karten Finder")
+
 
 class Language(Enum):
     englisch = 1
@@ -99,9 +108,12 @@ def get_Spalten(df_Expansion, cardNumber, cardName, cardCondition, possibleList)
         cardNumberInt = get_CardNumber(cardNumber)
         
         if(cardNumberInt == -1):
-            add_Hinweis(f"Die Karte <strong>{cardName}</strong> hat keine Kartennummer: <strong>{cardNumber}</strong> mögliche Boxen: <strong>{', '.join(map(str, get_IndexFromPossibleList(df_Expansion,possibleList, 'Box')))}</strong> Fach: <strong>{', '.join(map(str, ([int(x) for x in get_IndexFromPossibleList(df_Expansion,possibleList, 'Fach')])))}</strong>", "H1")
-            hinweis = True
-            return -1,-1,hinweis
+            if(len(possibleList) == 1):
+                return spalten[possibleList[i]], box[possibleList[i]], hinweis
+            else:
+                add_Hinweis(f"Die Karte <strong>{cardName}</strong> hat keine Kartennummer: <strong>{cardNumber}</strong> mögliche Boxen: <strong>{', '.join(map(str, get_IndexFromPossibleList(df_Expansion,possibleList, 'Box')))}</strong> Fach: <strong>{', '.join(map(str, ([int(x) for x in get_IndexFromPossibleList(df_Expansion,possibleList, 'Fach')])))}</strong>", "H1")
+                hinweis = True
+                return -1,-1,hinweis
 
         if pd.isna(condition.iloc[possibleList[i]]) or condition.iloc[possibleList[i]] == "" or condition.iloc[possibleList[i]] == "NM":
             if (cardNumberInt >= int(spalteVon[possibleList[i]]) and cardNumberInt <= int(spalteBis[possibleList[i]])):
@@ -154,8 +166,10 @@ def get_List(df_Expansion, sellingCardsExpansion, sellingCardsLanguage, sellingC
             returnList.insert(1, listWithLanguage[i])
         for i in range(len(listWithCondtion)):
              returnList.insert(1, listWithCondtion[i])
- 
-    return returnList, fehler
+    
+    unique = list(set(returnList))
+
+    return unique, fehler
 
 def get_getColumnsFromDFWithList(list, df):
     temp_df = pd.DataFrame(columns=df.columns) 
@@ -197,7 +211,7 @@ def mainSortFunction(df_Cards, df_Expansion):
 
             if(hinweis):
                sellingCardsHinweis[i] = hinweisList[len(hinweisList) - 1]["code"]
-               CardsWithHint.append({"PossibleList": possibleExpansionList ,"ID": i})
+               CardsWithHint.append({"PossibleList": possibleExpansionList ,"ID": i, "Gefunden": False})
 
             if(spalte != -1 and box != -1):
                 sellingCardsBox[i] = int(box)
@@ -231,28 +245,61 @@ def showCardsWithHint(df_Cards, df_Expansion):
     for i in range(len(CardsWithHint)): 
         st.markdown(f"<span style='font-size:30px;'>{sellingCardsName.loc[CardsWithHint[i]['ID']]}</span>", unsafe_allow_html=True)
         
-        temp_df = df_Cards.iloc[[CardsWithHint[i]['ID']]]
+        CardsWithHint[i]["Gefunden"] = st.checkbox("Gefunden " + sellingCardsName.loc[CardsWithHint[i]['ID']], value=False)
+        
+        if CardsWithHint[i]["Gefunden"] == False:
+            temp_df = df_Cards.iloc[[CardsWithHint[i]['ID']]]
 
-        temp_df = ColumnOutputForBoxes(temp_df)
+            temp_df = ColumnOutputForBoxes(temp_df)
             
-        posList = CardsWithHint[i]['PossibleList']
+            posList = CardsWithHint[i]['PossibleList']
         
-        st.dataframe(temp_df)
+            st.dataframe(temp_df)
 
-        st.markdown("<span style='font-size:25px;'>Mögliche Boxen:</span>", unsafe_allow_html=True)
+            st.markdown("<span style='font-size:25px;'>Mögliche Boxen:</span>", unsafe_allow_html=True)
         
-        resultList = []
-        for a in range(len(posList)):
-            resultList.append({"Box":box[posList[a]], "Spalte": spalten[posList[a]]})
+            resultList = []
+            for a in range(len(posList)):
+                resultList.append({"Box":box[posList[a]], "Spalte": spalten[posList[a]]})
         
-        df = pd.DataFrame(resultList)
-        df = df.drop_duplicates()
-        resultList = df.to_dict(orient="records")
+            df = pd.DataFrame(resultList)
+            df = df.drop_duplicates()
+            resultList = df.to_dict(orient="records")
 
-        for a in range(len(resultList)):
-            st.markdown(f"<span style='font-size:20px;'>Box:  {str(resultList[a]['Box'])} Spalte: {str(resultList[a]['Spalte'])}</span>",unsafe_allow_html=True) 
+            for a in range(len(resultList)):
+                st.markdown(f"<span style='font-size:20px;'>Box:  {str(resultList[a]['Box'])} Spalte: {str(resultList[a]['Spalte'])}</span>",unsafe_allow_html=True) 
 
+def showCardsWithHintFound(df_Cards, df_Expansion):
+    st.title("Gefunde Hinweis Karten:")
 
+    sellingCardsName = df_Cards["Localized Article Name"]
+    spalten = df_Expansion["Fach"]   
+    box = df_Expansion["Box"]   
+
+    for i in range(len(CardsWithHint)): 
+        if CardsWithHint[i]["Gefunden"] == True:
+            st.markdown(f"<span style='font-size:30px;'>{sellingCardsName.loc[CardsWithHint[i]['ID']]}</span>", unsafe_allow_html=True)
+        
+            temp_df = df_Cards.iloc[[CardsWithHint[i]['ID']]]
+
+            temp_df = ColumnOutputForBoxes(temp_df)
+            
+            posList = CardsWithHint[i]['PossibleList']
+        
+            st.dataframe(temp_df)
+
+            st.markdown("<span style='font-size:25px;'>Mögliche Boxen:</span>", unsafe_allow_html=True)
+        
+            resultList = []
+            for a in range(len(posList)):
+                resultList.append({"Box":box[posList[a]], "Spalte": spalten[posList[a]]})
+        
+            df = pd.DataFrame(resultList)
+            df = df.drop_duplicates()
+            resultList = df.to_dict(orient="records")
+
+            for a in range(len(resultList)):
+                st.markdown(f"<span style='font-size:20px;'>Box:  {str(resultList[a]['Box'])} Spalte: {str(resultList[a]['Spalte'])}</span>",unsafe_allow_html=True) 
 
 def getCardsSortedToBoxes(df_Cards):
     gefundene_BoxSpalte = df_Cards[["Box", "Fach"]].drop_duplicates()
@@ -329,19 +376,19 @@ def showBoxesGefunden(boxList):
     st.title("Gefundene Karten:")
 
     for i in range(len(boxList)): 
-        st.markdown(
+        if boxList[i]["Gefunden"] == True:
+            st.markdown(
                 f"<span style='font-size:30px;'>Box: {int(boxList[i]['Box'])} - {int(boxList[i]['Spalte'])}</span>",
                 unsafe_allow_html=True
             )
         
-        if boxList[i]["Gefunden"] == True:
             df = boxList[i]["df"].copy()
             df.index = df.index + 1
             st.dataframe(df)
                   
     return boxList
 
-def getCardsSortedToBestellungen(df_Cards):
+def getCardsSortedToBestellungen(df_Cards, cut = True):
     spaltenname = get_CurrentLanguageRowText()
     
     bestellungOrderSpalte = df_Cards[[spaltenname, "Order ID"]].drop_duplicates()
@@ -361,7 +408,8 @@ def getCardsSortedToBestellungen(df_Cards):
         temp_df = get_getColumnsFromDFWithList(bestellungList,df_Cards)
        
         if(len(bestellungList) > 0):
-            temp_df = ColumnOutputForBestellungen(temp_df)
+            if(cut):
+                temp_df = ColumnOutputForBestellungen(temp_df)
 
             returnList.append({"Bestellung": bestellungsID ,"OrderID": order, "df": temp_df})
     return returnList
@@ -409,6 +457,240 @@ def showBestellungenResults(list):
         df = list[i]["df"].copy()
         df.index = df.index + 1
         st.dataframe(df.style.apply(highlight_combined, axis=1))
+
+def ColumnOutputForPDF(temp_df):
+    st.write(temp_df)
+
+    spalteTemp = temp_df.pop(temp_df.columns[2])
+
+    temp_df.insert(0,"Stückzahl",spalteTemp)
+
+    spalteTemp = temp_df.pop(temp_df.columns[6])
+
+    temp_df.insert(15,"Price",spalteTemp)
+
+    temp_df = temp_df.drop("Product ID", axis=1)
+    temp_df = temp_df.drop("Order ID", axis=1)
+    temp_df = temp_df.drop("Rarity", axis=1)
+    temp_df = temp_df.drop("Box", axis=1)
+    temp_df = temp_df.drop("Fach", axis=1)
+    temp_df = temp_df.drop("Expansion", axis=1)
+    temp_df = temp_df.drop(get_CurrentLanguageRowText(), axis=1)
+    temp_df = temp_df.drop("Fehlermeldung", axis=1)
+    temp_df = temp_df.drop("Hinweis", axis=1)
+    temp_df = temp_df.drop("isFoil", axis=1)
+    temp_df = temp_df.drop("isSigned", axis=1)
+    temp_df = temp_df.drop("isAltered", axis=1)
+    temp_df = temp_df.drop(temp_df.columns[1], axis = 1)
+
+    spalteTemp = temp_df.pop("Collector Number")
+
+    temp_df.insert(2,"Collector Number",spalteTemp)
+
+    spalteTemp = temp_df.pop("Language")
+
+    temp_df.insert(3,"Language",spalteTemp)
+
+    spalteTemp = temp_df.pop("Comments")
+
+    temp_df.insert(5,"Comments",spalteTemp)
+
+    st.write(temp_df)
+
+    return temp_df
+def getAllLinks(list):
+    order_urls = []
+
+    for i in range(len(list)): 
+        linkText =  "https://www.cardmarket.com/en/Magic/Orders/" + str(list[i]['OrderID'])
+        order_urls.append(linkText)
+    
+    return order_urls
+
+def getShipping(url, driver):
+    driver.get(url)
+    time.sleep(0.5)
+    
+    try:
+        div = driver.find_element(By.CSS_SELECTOR, "div.text-danger")
+        full_text = div.text
+
+        first_part = full_text.split("|")[1].strip()
+        
+        dd = driver.find_element(By.CSS_SELECTOR, "dd.col-12.col-md-8.col-lg-9")
+
+        spans = dd.find_elements(By.TAG_NAME, "span")
+
+        brief_text = spans[1].text     
+
+        return {
+            "Method": brief_text,
+            "Trust": first_part,
+        }
+    except:
+        return {"Fehler": "❌ Adresse nicht gefunden"}
+
+def getPayment(url, driver):
+    driver.get(url)
+    time.sleep(0.5)
+    
+    try:
+        summary = driver.find_element(By.CSS_SELECTOR, ".summary")
+    
+        article_count = summary.get_attribute("data-article-count")
+        item_value = summary.get_attribute("data-item-value")
+        shipping_price = summary.get_attribute("data-shipping-price")
+        total_price = summary.get_attribute("data-total-price")
+
+        return {
+            "Contents": article_count,
+            "Article Value": item_value,
+            "Shipping": shipping_price,
+            "Total": total_price,
+        }
+    except:
+        return {"Fehler": "❌ Adresse nicht gefunden"}
+
+def getAddress(url, driver):
+    driver.get(url)
+    time.sleep(7) 
+
+    try:
+        toggle_button = driver.find_element(By.CSS_SELECTOR, "#labelShippingAddress button")
+        toggle_button.click()
+        time.sleep(1)
+    except:
+        print()
+
+    try:
+        name = driver.find_element(By.CSS_SELECTOR, "#ShippingAddress .Name").text
+        street = driver.find_element(By.CSS_SELECTOR, "#ShippingAddress .Street").text
+        city = driver.find_element(By.CSS_SELECTOR, "#ShippingAddress .City").text
+        country = driver.find_element(By.CSS_SELECTOR, "#ShippingAddress .Country").text
+        user_link_element = driver.find_element(By.CSS_SELECTOR, 'a[href*="/en/Magic/Users/"]')
+
+        return {
+            "User": user_link_element.text,
+            "Empfänger": name,
+            "Straße": street,
+            "Stadt": city,
+            "Land": country
+        }
+    except:
+        return {"Fehler": "❌ Adresse nicht gefunden"}
+
+def getData(list, i):
+    order_urls = getAllLinks(list)
+ 
+    # Selenium setup
+    options = Options()
+    options.headless = False  # Sichtbares Fenster für Login
+    driver = webdriver.Chrome(options=options)
+
+    url = order_urls[i]
+
+    resultAddress = getAddress(url, driver)
+    resultPayment = getPayment(url, driver)
+    resultShipping = getShipping(url, driver)
+
+    driver.quit()
+
+    return resultAddress, resultPayment, resultShipping
+
+def draw_table(c, data, x_start=45, y_start=500, row_height=14):
+    for row_num, row in enumerate(data):
+        if(row_num != 0):  
+            start = x_start
+            for col_num, cell in enumerate(row):
+                addval = 0
+                if(col_num == 0):
+                   addval = 0
+                if(col_num == 1):
+                    addval = 15 
+                if(col_num == 2):
+                    addval = 225
+                if(col_num == 3):
+                    addval = 25
+                if(col_num == 4):
+                    addval = 50
+                if(col_num == 5):
+                    addval = 25
+                if(col_num == 6):
+                    addval = 150
+
+                start += addval
+                c.drawString(start, y_start - row_num * row_height, str(cell)) 
+
+    
+
+def createPDFs(list):
+    if(st.button("Load Data")):
+        pdfs = []
+        for i in range(len(list)): 
+            temp = ("Sale #" + str(list[i]['OrderID']))
+            pdfs.append(temp)
+            c = canvas.Canvas(pdfs[i])
+            c.setFont("Helvetica", 15)
+            c.drawString(45, 790, str(temp))
+
+            address, payment, shipping = getData(list, i)
+            
+            c.setFont("Helvetica", 10)
+
+            message = "Buyer - " + str(address["User"])
+            c.drawString(305, 734, message)
+
+            c.setFont("Helvetica", 10)
+            a = 0
+
+            for key, value in address.items():
+                if (key != "User"):
+                    c.drawString(305, 720 - (14 * a), str(value))
+                    a += 1
+            
+            c.drawString(45, 734, "Seller - mimtelf")
+            c.drawString(45, 720, "Mario Milanollo")
+            c.drawString(45, 706, "Neuhausgasse 1")
+            c.drawString(45, 692, "7202 Bad Sauerbrunn")
+            c.drawString(45, 678, "Austria")
+
+            c.drawString(45, 660, "Contents")
+            c.drawString(45, 646, "Article Value")
+            c.drawString(45, 632, "Shipping")
+            c.drawString(45, 618, "Total")
+
+            c.drawString(130, 660, str(payment["Contents"]) + " articles")
+            c.drawString(130, 646, str(payment["Article Value"]) + " EUR")
+            c.drawString(130, 632, str(payment["Shipping"]) + " EUR")
+            c.drawString(130, 618, str(payment["Total"]) + " EUR")
+
+            c.drawString(130, 632, str(payment["Shipping"]) + " EUR")
+            c.drawString(130, 618, str(payment["Total"]) + " EUR")
+
+            c.drawString(45, 590, "Shipping Method " + str(shipping["Method"]))
+            c.drawString(45, 576, str(shipping["Trust"]))
+
+            df = list[i]["df"].copy()
+            df.index += 1
+            df = ColumnOutputForPDF(df)
+            data = [df.columns.tolist()] + df.values.tolist()
+
+            draw_table(c, data, x_start=45, y_start=500)
+            c.save()
+        
+        merger = PdfMerger()
+        for pdf in pdfs:
+            merger.append(pdf)
+        output_path = "merged_output.pdf"
+        merger.write(output_path)
+        merger.close()
+
+        # Load final PDF into memory
+        with open(output_path, "rb") as f:
+            pdf_data = f.read()
+
+        # Provide download button
+        st.download_button("Download PDF", data=pdf_data, file_name="merged_output.pdf", mime="application/pdf")
 
 expansion_csv = st.file_uploader("Expansion- und Boxenliste hochladen", type="csv")
 sellingCards_csv = st.file_uploader("Gesuchte Karten hochladen", type="csv")
@@ -496,8 +778,13 @@ if sellingCards_csv and expansion_csv:
     bestellungenList = getCardsSortedToBestellungen(df_CardsCopy)
 
     showBestellungenResults(bestellungenList)
-  
-    boxList = showBoxesGefunden(boxList)
+    
+    bestellungenList = getCardsSortedToBestellungen(df_CardsCopy, False)
 
+    createPDFs(bestellungenList)
+
+    boxList = showBoxesGefunden(boxList)
+    showCardsWithHintFound(df_CardsCopy,df_Expansion)
 #streamlit run Magic_Card_Finder.py
 #cd C:\Users\ismae\Desktop\Magic 
+# 11enmoney4Us!
